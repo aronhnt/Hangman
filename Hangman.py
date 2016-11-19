fd3ee035e1115c03b07e0c5ebc4f842f09f2ee67
AaRoN <aaron.hunt@bourne-grammar.lincs.sch.uk>
2016-11-19 20:51:36 +0000

    Added Python File
    Added the start of my code, please note this is far from finished and unless you enjoy running a menu, I would download one of my other programs :)

---
+++ Hangman.py
@@ -0,0 +1,36 @@
+#Hangman Game
+
+def menu():
+    running = True
+    while running == True:
+        print('''
+1) Play Game
+2) Quit''')
+        option = input('Choose An Option: ')
+        option = errorCheck(option,int,'Choose An Option: ')
+        if option == 1:
+            print('This Game Is Currently a Work In Progress, Please Follow @aronhnt on github for updates')
+        elif option == 2:
+            running = False
+
+def main():
+    '''For this function, i will need to have:
+Number of incorrect attempts - 8
+Displaying Empty word e.g. _ _ _ _ _ to user and then adding a letter as guessed
+Lists of words - import a file?
+Not losing a life if letter guessed twice
+allowing only one letter to be entered e.g. aa would not be allowed
+other stuff i cant remember'''
+
+    
+def errorCheck(varName, type, question):
+    x=0
+    while x == 0:
+        try:
+            varName=type(varName)
+            x=1
+        except ValueError:
+            varName = input(question)
+    return varName
+
+menu()
